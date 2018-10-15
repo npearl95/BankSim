ddfc27e06143fb46f70f9e64b8ee5ac8ef37e6de
tug23261 <ishan@temple.edu>
2018-10-14 20:01:56 -0400

    lab 5(Complete)

--- src/edu/temple/cis/c3238/banksim/BankSimMain.java
+++ src/edu/temple/cis/c3238/banksim/BankSimMain.java
@@ -1,5 +1,5 @@
 package edu.temple.cis.c3238.banksim;
-
+import java.util.concurrent.locks.*;
 /**
  * @author Cay Horstmann
  * @author Modified by Paul Wolfgang
@@ -9,14 +9,27 @@ public class BankSimMain {
 
     public static final int NACCOUNTS = 10;
     public static final int INITIAL_BALANCE = 10000;
+    private static final Lock t_lock = new ReentrantLock();
 
     public static void main(String[] args) {
+        
+        
         Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
+        
         Thread[] threads = new Thread[NACCOUNTS];
+        
         // Start a thread for each account
         for (int i = 0; i < NACCOUNTS; i++) {
-            threads[i] = new TransferThread(b, i, INITIAL_BALANCE);
-            threads[i].start();
+            t_lock.lock();
+            try{
+                threads[i] = new TransferThread(b, i, INITIAL_BALANCE);
+                threads[i].start();
+            }
+            finally{
+                t_lock.unlock();
+            }
+            
+            
         }
 
 //        b.test();
